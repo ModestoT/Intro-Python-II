ca6a264f6b71cf5d2a1c25f531aae7e7781546fb
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-04-24 12:54:42 -0500

    set up basic Room class

--- src/room.py
+++ src/room.py
@@ -1,2 +1,17 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+  def __init__(self, name, desc):
+    self.name = name
+    self.desc = desc
+    self.n_to = ''
+    self.s_to = ''
+    self.w_to = ''
+    self.e_to = ''
+  
+  def __repr__(self):
+    return ''+self.name+': '+self.desc+''
+
+  def __str__(self):
+    return ''+self.name+': '+self.desc+''
