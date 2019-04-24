ff51138fc1a932c67ff09fba698d66f43da32251
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-04-24 12:59:21 -0500

    created a basic player class

--- src/player.py
+++ src/player.py
@@ -1,2 +1,14 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+  def __init__(self, name, current_room):
+    self.name = name
+    self.current_room = current_room
+    self.inventory = []
+
+  def __repr__(self):
+    return ''+self.name+''
+
+  def __str__(self):
+    return ''+self.name+''
